380f31b3ae3b0448a8f76613a8544486c69f6526
Wojciech Zarski <wojciech.zarski@gmail.com>
2018-06-09 18:33:19 +0200

    Publish 1.0-SNAPSHOT documentation

--- 1.0-SNAPSHOT/javadocs/type-search-index.js
+++ 1.0-SNAPSHOT/javadocs/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"p":"net.zarski.conference.ci.plantshop","l":"Session"},{"p":"net.zarski.conference.ci.plantshop","l":"Shop"},{"p":"net.zarski.conference.ci.plantshop","l":"ShoppingCart2"}]
+typeSearchIndex = [{"p":"net.zarski.conference.ci.plantshop","l":"Session"},{"p":"net.zarski.conference.ci.plantshop","l":"Shop"},{"p":"net.zarski.conference.ci.plantshop","l":"ShoppingCart"}]
